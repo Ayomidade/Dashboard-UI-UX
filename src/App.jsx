8bd0113a7cb13fec30b405242ed2da8873974782
cosmosayo <alfredchrisayo@gmail.com>
2025-10-14 13:47:22 +0100

    initialized state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import Home from './page/Home'
-import "./App.css"
+import Home from "./page/Home";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <Home/>
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
