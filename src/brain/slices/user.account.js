7eaaa50c3fd2dcd27b761cf099825255e3c27100
cosmosayo <alfredchrisayo@gmail.com>
2025-10-20 09:33:09 +0100

    save

--- src/brain/slices/user.account.js
+++ src/brain/slices/user.account.js
@@ -1,16 +1,1 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-export const userSlice = createSlice({
-  name: "user",
-  initialState: {
-    metaData: {
-      Fullname: "John Doe",
-      password: "pasword!234",
-      email: "John@doe.info",
-    },
-    settings: {},
-  },
-  reducers: {},
-});
-
-export default userSlice.reducer;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import React, { StrictMode } from "react";
 import store from "./brain/store.js";
 import { BrowserRouter } from "react-router-dom";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
